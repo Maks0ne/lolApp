a05b0838c8af913103720e435ceffcf33d32774d
Maksim Chystiakov <maks.goodluck23@gmail.com>
2024-04-14 22:21:17 +0200

    update

--- src/config/RiotApi.js
+++ src/config/RiotApi.js
@@ -1,7 +1,7 @@
 
-export const imgApi = 'http://ddragon.leagueoflegends.com/cdn/img/champion/loading/'
-export const charApi = 'http://ddragon.leagueoflegends.com/cdn/14.7.1/data/en_US/champion.json'
-export const passiveApi = 'http://ddragon.leagueoflegends.com/cdn/14.7.1/img/passive/'
-export const skillsApi = 'http://ddragon.leagueoflegends.com/cdn/14.7.1/img/spell/'
+export const imgApi = 'https://ddragon.leagueoflegends.com/cdn/img/champion/loading/'
+export const charApi = 'https://ddragon.leagueoflegends.com/cdn/14.7.1/data/en_US/champion.json'
+export const passiveApi = 'https://ddragon.leagueoflegends.com/cdn/14.7.1/img/passive/'
+export const skillsApi = 'https://ddragon.leagueoflegends.com/cdn/14.7.1/img/spell/'
 
 
